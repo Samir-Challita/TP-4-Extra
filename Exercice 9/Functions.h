7a5a33f09a7b655ae5d59edc5c670340fe84d427
Samir Challita <159939271+Samir-Challita@users.noreply.github.com>
2025-12-01 20:17:56 +0200

    Update Functions.h

--- Exercice 9/Functions.h
+++ Exercice 9/Functions.h
@@ -14,24 +14,15 @@ int parfait(int x) {
 		return 0;
 }
 
-
-
-
-
-
-
-
-
-
-//int premier(int x) {
-//	int i, S = 0;
-//	for(i = 2; i <= (x / 2); i++) {
-//		if (x % i == 0) {
-//			break;
-//		}
-//		if (i > x / 2)
-//			return 1;
-//		else
-//			return 0;
-//	}
-//}
+/*int premier(int x) {
+	int i, S = 0;
+	for(i = 2; i <= (x / 2); i++) {
+		if (x % i == 0) {
+			break;
+		}
+		if (i > x / 2)
+			return 1;
+		else
+			return 0;
+	}
+}*/
